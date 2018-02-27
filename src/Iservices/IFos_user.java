b5db6fc335559ab0a064b031270e81e10002c514
Wajih <mohamedwajih.hamza@esprit.tn>
2018-02-27 19:57:08 +0100

    333

---
+++ src/Iservices/IFos_user.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IServices;
+
+import Entities.Fos_user;
+import java.util.List;
+
+/**
+ *
+ * @author hp
+ */
+public interface IFos_user {
+
+    public void ajouter_utilisateur(Fos_user u);
+
+    public void supprimer_utilisateur(Fos_user u);
+
+    public void modifier_utilisateur(Fos_user u);
+
+    public void ajouter_responsable_event(Fos_user u);
+
+    public void modifier_responsable_event(Fos_user u);
+
+    public void modifier_mdp(Fos_user u);
+
+    public List<Fos_user> afficher_responsable_event();
+
+    public List<Fos_user> afficher_user();
+
+    public List<Fos_user> chercher_user_par_nom_prenom(String nom, String prenom);
+
+    public List<Fos_user> chercher_user_par_nom(String nom);
+
+    public List<Fos_user> chercher_user_par_prenom(String prenom);
+
+    public void activerCompte(int id);
+
+    public boolean Authentifier(String login, String password);
+
+    public boolean verifCompte(int id);
+
+}
