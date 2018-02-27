ad71238c353880d9fb2552f088d9a31706884a54
Wajih <mohamedwajih.hamza@esprit.tn>
2018-02-27 23:32:51 +0100

    cc

--- src/Presentation/NewFXMain.java
+++ src/Presentation/NewFXMain.java
@@ -31,7 +31,7 @@ public class NewFXMain extends Application {
    public void start (Stage stage) throws IOException
 
    {   
-       Parent  root = FXMLLoader.load(getClass().getResource("MatchAdmin.fxml"));
+       Parent  root = FXMLLoader.load(getClass().getResource("TestPersonnalite.fxml"));
        Scene scene = new Scene(root);
        stage.setScene(scene);
        stage.show();
